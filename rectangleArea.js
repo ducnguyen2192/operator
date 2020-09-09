c90d6622f33c87a641f85b14b44d0a53c5ad6548
duc nguyen <ducnguyen2192@gmail.com>
2020-09-09 13:49:43 +0700

    Add rectangleArea.html rectangleArea.js

---
+++ rectangleArea.js
@@ -0,0 +1,8 @@
+function calArea(){
+    let inputHeight = prompt("input height");
+    let inputWidth = prompt("input width");
+    let h = parseInt(inputHeight);
+    let w = parseInt(inputWidth);
+    var a = h*w;
+    alert("Area is " + a)
+}
